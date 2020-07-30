452850f0bbccf10c6905f1ffc63a109bbb1f64c6
lfbox7 <lfbox7@yahoo.com>
2020-07-29 19:07:48 -0500

    initial commit

--- controller/pages.js
+++ controller/pages.js
@@ -2,17 +2,33 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const ct1product = require('../models/ct1');
-const ct1 = require('../models/ct1');
+const bakingItems = require('../models/bakingItems');
+
 
 const router = express.Router();
 
-
-
 router.get('/', (req, res) => {
-    ct1product.find()
+    bakingItems.find()
         .then (result => {
-            res.render('index', {products: result, pageTitle: 'Homepage'});
+            console.log(result);
+            res.render('index', {products: result, pageTitle: 'Homepage', pageName: 'Baking Items'});
+        })
+        .catch(err => console.log(err));
+});
+
+
+router.get('/baking-items', (req, res) => {
+    bakingItems.find()
+        .then (result => {
+            res.render('baking-items', {products: result, pageTitle: 'Baking Items', pageName: 'Home'});
+        })
+        .catch(err => console.log(err));
+});
+
+router.get('/bakingItems/:prodId', (req, res) => {
+    backingItems.findById(req.params.prodId)
+        .then (result => {
+            res.render('product-detail', {prod: result, pageTitle: 'Product Detail', pageName: 'Baking Items'});
         })
         .catch(err => console.log(err));
 });
---
+++ models/bakingItems.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bakingItemsSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    description: {
+        type: String
+    },
+    instructions: {
+        type: String
+    },
+    ingredients: {
+        type: String
+    },
+    quantity: {
+        type: Number
+    }
+
+})
+module.exports = mongoose.model('bakingItems', bakingItemsSchema);
