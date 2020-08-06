971fc1589804bba634e1fb54b6a1a72074d41ad0
lfbox7 <lfbox7@yahoo.com>
2020-08-06 12:31:18 -0500

    initial commit

--- controller/pages.js
+++ controller/pages.js
@@ -13,42 +13,46 @@ const WishList = require('../models/wish-list');
 
 const router = express.Router();
 
-
-
 router.get('/', (req, res) => { // homepage display 
-    ct1product.find()
-        .then (result => {
-            res.render('index', {product: result, pageTitle: 'Fresh Produce', pageName: 'Fresh Produce'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct1product.find()
+            .then (result => {
+                res.render('index', {product: result, cart: cart, pageTitle: 'Fresh Produce', pageName: 'Fresh Produce'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.post('/add-cart', (req, res) => { // inserting items from cart 
     const itemId = req.body.id;
     if (req.body.catId == 'cat1') {
         ct1product.findById(itemId)
-            .then(result => { insertIntoCart(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoCart(result, itemId); })
+        .catch(err => console.log(err));
     } else if (req.body.catId == 'cat2') {
         ct2product.findById(itemId)
-            .then(result => { insertIntoCart(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoCart(result, itemId); })
+        .catch(err => console.log(err));
     } else if (req.body.catId == 'cat3') {
         ct3product.findById(itemId)
-            .then(result => { insertIntoCart(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoCart(result, itemId); })
+        .catch(err => console.log(err));
     } else if (req.body.catId == 'cat4') {
         ct4product.findById(itemId)
-            .then(result => { insertIntoCart(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoCart(result, itemId); })
+        .catch(err => console.log(err));
     } else if (req.body.catId == 'cat5') {
         ct5product.findById(itemId)
-            .then(result => { insertIntoCart(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoCart(result, itemId); })
+        .catch(err => console.log(err));
     } else {
         ct6product.findById(itemId)
-            .then(result => { insertIntoCart(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoCart(result, itemId); })
+        .catch(err => console.log(err));
     }
 });
 
@@ -65,19 +69,19 @@ function insertIntoCart(result, itemId) {
 router.delete('/:id', (req, res) => {
     Cart.findByIdAndDelete(req.params.id, function(err, doc) { if(err) {console.log(err); } });
     Cart.find()
-        .then (result => {
-            res.render('cart', {product: result, pageTitle: 'cart', pageName: 'Cart', status: 0});
-        })
-        .catch(err => console.log(err));    
+    .then (result => {
+        res.render('cart', {cart: result, pageTitle: 'cart', pageName: 'Cart', status: 0});
+    })
+    .catch(err => console.log(err));    
 });
 
 router.get('/increase/:id', (req, res) => {
     Cart.findByIdAndUpdate({_id: req.params.id}, {$inc: {quantity: 1}}, function(err, doc) { if(err) {console.log(err);} })
     Cart.find()
-        .then (result => {
-            res.render('cart', {product: result, pageTitle: 'cart', pageName: 'Cart', status: 1});
-        })
-        .catch(err => console.log(err)); 
+    .then (result => {
+        res.render('cart', {cart: result, pageTitle: 'cart', pageName: 'Cart', status: 1});
+    })
+    .catch(err => console.log(err)); 
 });
 
 router.get('/decrease/:id', (req, res) => {
@@ -85,53 +89,54 @@ router.get('/decrease/:id', (req, res) => {
         if (doc.quantity == 1) {
             Cart.findByIdAndDelete(req.params.id, function(err, doc) { if(err) {console.log(err); } });
             Cart.find()
-                .then (result => {
-                    res.render('cart', {product: result, pageTitle: 'cart', pageName: 'Cart', status: 1});
-                })
-                .catch(err => console.log(err));
+            .then (result => {
+                res.render('cart', {cart: result, pageTitle: 'cart', pageName: 'Cart', status: 1});
+            })
+            .catch(err => console.log(err));
         } else {
             Cart.find()
-                .then (result => {
-                    res.render('cart', {product: result, pageTitle: 'cart', pageName: 'Cart', status: 1});
-                })
-                .catch(err => console.log(err));
-        }})
+            .then (result => {
+                res.render('cart', {cart: result, pageTitle: 'cart', pageName: 'Cart', status: 1});
+            })
+            .catch(err => console.log(err));
+        }
+    })
 });
 
 router.get('/cart', (req, res) => { // retrieving items from cart
     Cart.find()
-        .then (result => {
-            res.render('cart', {product: result, pageTitle: 'cart', pageName: 'Cart', status: 0});
-        })
-        .catch(err => console.log(err));
+    .then (result => {
+        res.render('cart', {cart: result, pageTitle: 'cart', pageName: 'Cart', status: 0});
+    })
+    .catch(err => console.log(err));
 });
 
 router.post('/add-wish-list', (req, res) => {
     const itemId = req.body.id;
     if (req.body.catId == 'cat1') {
         ct1product.findById(itemId)
-            .then(result => { insertIntoWishList(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoWishList(result, itemId); })
+        .catch(err => console.log(err));
     } else if (req.body.catId == 'cat2') {
         ct2product.findById(itemId)
-            .then(result => { insertIntoWishList(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoWishList(result, itemId); })
+        .catch(err => console.log(err));
     } else if (req.body.catId == 'cat3') {
         ct3product.findById(itemId)
-            .then(result => { insertIntoWishList(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoWishList(result, itemId); })
+        .catch(err => console.log(err));
     } else if (req.body.catId == 'cat4') {
         ct4product.findById(itemId)
-            .then(result => { insertIntoWishList(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoWishList(result, itemId); })
+        .catch(err => console.log(err));
     } else if (req.body.catId == 'cat5') {
         ct5product.findById(itemId)
-            .then(result => { insertIntoWishList(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoWishList(result, itemId); })
+        .catch(err => console.log(err));
     } else {
         ct6product.findById(itemId)
-            .then(result => { insertIntoWishList(result, itemId); })
-            .catch(err => console.log(err));
+        .then(result => { insertIntoWishList(result, itemId); })
+        .catch(err => console.log(err));
     }
 });
 
@@ -146,148 +151,252 @@ function insertIntoWishList(result, itemId) {
 
 router.delete('/:id', (req, res) => {
     WishList.findByIdAndDelete(req.params.id, function(err, doc) { if(err) {console.log(err); } });
-    WishList.find()
-        .then (result => {
-            res.render('wish-list', {product: result, pageTitle: 'wish list', pageName: 'Wish List', status: 0});
-        })
-        .catch(err => console.log(err));    
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            WishList.find()
+            .then (result => {
+                res.render('wish-list', {wishList: result, cart: cart, pageTitle: 'wish list', pageName: 'Wish List', status: 0});
+            })
+            .catch(err => console.log(err)); 
+        }
+    });   
 });
 
 router.get('/increase/:id', (req, res) => {
     WishList.findByIdAndUpdate({_id: req.params.id}, {$inc: {quantity: 1}}, function(err, doc) { if(err) {console.log(err);} })
-    WishList.find()
-        .then (result => {
-            res.render('wish-list', {product: result, pageTitle: 'wish list', pageName: 'Wish List', status: 0});
-        })
-        .catch(err => console.log(err)); 
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            WishList.find()
+            .then (result => {
+                res.render('wish-list', {wishList: result, cart: cart, pageTitle: 'wish list', pageName: 'Wish List', status: 0});
+            })
+            .catch(err => console.log(err)); 
+        }
+    });
 });
 
 router.get('/decrease/:id', (req, res) => {
     WishList.findByIdAndUpdate({_id: req.params.id}, {$inc: {quantity: -1}}, function(err, doc) { if(err) {console.log(err);} 
     if(doc.quantity == 1) {
         WishList.findByIdAndDelete(req.params.id, function(err, doc) { if(err) {console.log(err); } });
-        WishList.find()
-            .then (result => {
-                res.render('wish-list', {product: result, pageTitle: 'wish list', pageName: 'Wish List', status: 0});
-            })
-            .catch(err => console.log(err)); 
+        Cart.find({}, function(err, cart) {
+            if(err) {
+                console.log(err);
+            } else {
+                WishList.find()
+                .then (result => {
+                    res.render('wish-list', {wishList: result, cart: cart, pageTitle: 'wish list', pageName: 'Wish List', status: 0});
+                })
+                .catch(err => console.log(err));
+            }
+        });
     } else {
-        WishList.find()
-            .then (result => {
-                res.render('wish-list', {product: result, pageTitle: 'wish list', pageName: 'Wish List', status: 0});
-            })
-            .catch(err => console.log(err)); 
+        Cart.find({}, function(err, cart) {
+            if(err) {
+                console.log(err);
+            } else {
+                WishList.find()
+                .then (result => {
+                    res.render('wish-list', {wishList: result, cart:cart, pageTitle: 'wish list', pageName: 'Wish List', status: 0});
+                })
+                .catch(err => console.log(err));
+            }
+        });
     }})
 });
 
 router.get('/wish-list', (req, res) => {
-    WishList.find()
-        .then (result => {
-            res.render('wish-list', {product: result, pageTitle: 'wish list', pageName: 'Wish List', status: 0});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            WishList.find()
+            .then (result => {
+            res.render('wish-list', {wishList: result, cart: cart, pageTitle: 'wish list', pageName: 'Wish List', status: 0});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/contact', (req, res) => {
-            res.render('contact', {pageTitle: 'Contact'});
-    
+    Cart.find()
+    .then (result => {
+        res.render('contact', {cart: result, pageTitle: 'Contact'});
+    })
+    .catch(err => console.log(err));
 });
 
 router.get('/checkout', (req, res) => {
-    Cart.find()
-        .then (result => {
-            res.render('checkout', {product: result, pageTitle: 'checkout', pageName: 'Checkout', status: 0});
-        })
-        .catch(err => console.log(err));
-
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            Cart.find()
+            .then (result => {
+                res.render('checkout', {product: result, cart: cart, pageTitle: 'checkout', pageName: 'Checkout', status: 0});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/ct2', (req, res) => {
-    ct2product.find()
-        .then (result => {
-            res.render('ct2', {product: result, pageTitle: 'Cereal', pageName: 'Cereal'});
-        })
-        .catch(err => console.log(err));
-});''
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct2product.find()
+            .then (result => {
+                res.render('ct2', {product: result, cart: cart, pageTitle: 'Cereal', pageName: 'Cereal'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
+});
 
 router.get('/ct3', (req, res) => {
-    ct3product.find()
-        .then (result => {
-            res.render('ct3', {product: result, pageTitle: 'Dairy', pageName: 'Dairy'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct3product.find()
+            .then (result => {
+                res.render('ct3', {product: result, cart: cart, pageTitle: 'Dairy', pageName: 'Dairy'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/ct4', (req, res) => {
-    ct4product.find()
-        .then (result => {
-            res.render('ct4', {product: result, pageTitle: 'Everyday Items', pageName: 'Everyday Items'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct4product.find()
+            .then (result => {
+                res.render('ct4', {product: result, cart: cart, pageTitle: 'Everyday Items', pageName: 'Everyday Items'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/ct5', (req, res) => {
-    ct5product.find()
-        .then (result => {
-            res.render('ct5', {product: result, pageTitle: 'Breakfast Items', pageName: 'Breakfast Items'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct5product.find()
+            .then (result => {
+                res.render('ct5', {product: result, cart: cart, pageTitle: 'Breakfast Items', pageName: 'Breakfast Items'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/ct6', (req, res) => {
-    ct6product.find()
-        .then (result => {
-            res.render('ct6', {product: result, pageTitle: 'Baking Items', pageName: 'Baking Items'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct6product.find()
+            .then (result => {
+                res.render('ct6', {product: result, cart: cart, pageTitle: 'Baking Items', pageName: 'Baking Items'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/ct1/:prodId', (req, res) => {
-    ct1product.findById(req.params.prodId)
-        .then (result => {
-            res.render('product-detail1', {prod: result, pageTitle: 'Items Detail', pageName: 'Fresh Produce'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct1product.findById(req.params.prodId)
+            .then (result => {
+                res.render('product-detail1', {prod: result, cart: cart, pageTitle: 'Items Detail', pageName: 'Fresh Produce'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/ct2/:prodId', (req, res) => {
-    ct2product.findById(req.params.prodId)
-        .then (result => {
-            res.render('product-detail2', {prod: result, pageTitle: 'Items Detail', pageName: 'Cereal'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct2product.findById(req.params.prodId)
+            .then (result => {
+                res.render('product-detail2', {prod: result, cart: cart, pageTitle: 'Items Detail', pageName: 'Cereal'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/ct3/:prodId', (req, res) => {
-    ct3product.findById(req.params.prodId)
-        .then (result => {
-            res.render('product-detail3', {prod: result, pageTitle: 'Items Detail', pageName: 'Dairy'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct3product.findById(req.params.prodId)
+            .then (result => {
+                res.render('product-detail3', {prod: result, cart: cart, pageTitle: 'Items Detail', pageName: 'Dairy'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/ct4/:prodId', (req, res) => {
-    ct4product.findById(req.params.prodId)
-        .then (result => {
-            res.render('product-detail4', {prod: result, pageTitle: 'Items Detail', pageName: 'Everyday Items'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct4product.findById(req.params.prodId)
+            .then (result => {
+                res.render('product-detail4', {prod: result, cart: cart, pageTitle: 'Items Detail', pageName: 'Everyday Items'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/ct5/:prodId', (req, res) => {
-    ct5product.findById(req.params.prodId)
-        .then (result => {
-            res.render('product-detail5', {prod: result, pageTitle: 'Items Detail', pageName: 'Breakfast Items'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct5product.findById(req.params.prodId)
+            .then (result => {
+                res.render('product-detail5', {prod: result, cart: cart, pageTitle: 'Items Detail', pageName: 'Breakfast Items'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 router.get('/ct6/:prodId', (req, res) => {
-    ct6product.findById(req.params.prodId)
-        .then (result => {
-            res.render('product-detail6', {prod: result, pageTitle: 'Items Detail', pageName: 'Baking Items'});
-        })
-        .catch(err => console.log(err));
+    Cart.find({}, function(err, cart) {
+        if(err) {
+            console.log(err);
+        } else {
+            ct6product.findById(req.params.prodId)
+            .then (result => {
+                res.render('product-detail6', {prod: result, cart: cart, pageTitle: 'Items Detail', pageName: 'Baking Items'});
+            })
+            .catch(err => console.log(err));
+        }
+    });
 });
 
 module.exports = router;
