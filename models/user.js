69ad70d54c91f7acac9f7ed89f5ae36c5b246c6a
lfbox7 <lfbox7@yahoo.com>
2020-08-12 14:27:43 -0500

    initial commit

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    address: {
+        address1: { type: String },
+        address2: { type: String },
+        city: { type: String },
+        state: {type: String },
+        zipCode: { type: Number },
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+})
+module.exports = mongoose.model('user', userSchema);
